704b60dda0d40ccbc640b47d05b64955f67cf833
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-10-18 15:45:25 -0700

    adds content to why am I here and contact pages

--- script.js
+++ script.js
@@ -1,70 +1,19 @@
-const list1 = document.getElementById("dr");
-console.log(list1)
-list1.style.color = "whitesmoke";
-
-list1.addEventListener("mouseover", function handleMouseOver() {
-    list1.style.color = "red";
+const link1 = document.getElementById("email");
+console.log(link1)
+link1.style.color = "whitesmoke"
+link1.addEventListener("mouseover", function handleMouseOver() {
+    link1.style.color= "red";
 });
-list1.addEventListener("mouseout", function handleMouseOut() {
-    list1.style.color = "whitesmoke";
+link1.addEventListener("mouseout", function handleMouseOut() {
+    link1.style.color = "whitesmoke";
+})
+
+const link2 = document.getElementById("linkedIn");
+console.log(link2)
+link2.style.color = "whitesmoke"
+link2.addEventListener("mouseover", function handleMouseOver() {
+    link2.style.color= "red";
 });
-
-
-const list2 = document.getElementById("dd");
-console.log(list2)
-list2.style.color = "whitesmoke";
-
-list2.addEventListener("mouseover", function handleMouseOver() {
-    list2.style.color = "red";
-});
-list2.addEventListener("mouseout", function handleMouseOut() {
-    list2.style.color = "whitesmoke";
-});
-
-
-const list3 = document.getElementById("vg");
-console.log(list3)
-list3.style.color = "whitesmoke";
-
-list3.addEventListener("mouseover", function handleMouseOver() {
-    list3.style.color = "red";
-});
-list3.addEventListener("mouseout", function handleMouseOut() {
-    list3.style.color = "whitesmoke";
-});
-
-
-const list4 = document.getElementById("pc");
-console.log(list4)
-list4.style.color = "whitesmoke";
-
-list4.addEventListener("mouseover", function handleMouseOver() {
-    list4.style.color = "red";
-});
-list4.addEventListener("mouseout", function handleMouseOut() {
-    list4.style.color = "whitesmoke";
-});
-
-
-const list5 = document.getElementById("bc");
-console.log(list5)
-list5.style.color = "whitesmoke";
-
-list5.addEventListener("mouseover", function handleMouseOver() {
-    list5.style.color = "red";
-});
-list5.addEventListener("mouseout", function handleMouseOut() {
-    list5.style.color = "whitesmoke";
-});
-
-
-const list6 = document.getElementById("hi");
-console.log(list6)
-list6.style.color = "whitesmoke";
-
-list6.addEventListener("mouseover", function handleMouseOver() {
-    list6.style.color = "red";
-});
-list6.addEventListener("mouseout", function handleMouseOut() {
-    list6.style.color = "whitesmoke";
-});
+link2.addEventListener("mouseout", function handleMouseOut() {
+    link2.style.color = "whitesmoke";
+})
